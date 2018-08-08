4524a80c0d67707706f1dd29bf76908478d36dde
Rick van Veen <r.van.veen133@gmail.com>
2018-08-08 14:20:12 +0200

    Test commit

--- lvqtoolbox/__init__.py
+++ lvqtoolbox/__init__.py
@@ -1,5 +1,7 @@
 from .template import (TemplateEstimator, TemplateClassifier,
                        TemplateTransformer)
+from .glvqmodel import (GeneralizedLearningVectorQuantization)
+
 from . import template
 
 __all__ = ['TemplateEstimator', 'TemplateClassifier',
---
+++ lvqtoolbox/glvqmodel.py
@@ -0,0 +1,8 @@
+from sklearn.base import BaseEstimator
+
+
+class GeneralizedLearningVectorQuantization():
+	"""GeneralizedLearningVectorQuantization"""
+	def __init__(self, arg):
+		super(GeneralizedLearningVectorQuantization, self).__init__()
+		self.arg = arg
