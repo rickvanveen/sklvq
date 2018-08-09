81c4ca20cbc3f2cb4c9d774a0417eb6cdf5a71cf
Rick van Veen <r.van.veen133@gmail.com>
2018-08-09 14:33:25 +0200

    Removed template from module __init__

--- lvqtoolbox/__init__.py
+++ lvqtoolbox/__init__.py
@@ -1,9 +1,5 @@
-from .template import (TemplateEstimator, TemplateClassifier,
-                       TemplateTransformer)
 from .models import (GLVQ)
 
-from . import template
 from . import models
 
-__all__ = ['TemplateEstimator', 'TemplateClassifier',
-           'TemplateTransformer', 'GLVQ', 'template', 'models']
+__all__ = ['GLVQ', 'models']
--- lvqtoolbox/tests/test_common.py
+++ lvqtoolbox/tests/test_common.py
@@ -1,20 +1,7 @@
 from sklearn.utils.estimator_checks import check_estimator
-from lvqtoolbox import (TemplateEstimator, TemplateClassifier,
-                         TemplateTransformer)
+
 from lvqtoolbox import GLVQ
 
 
 def test_glvq():
-	return check_estimator(GLVQ)
-
-
-def test_estimator():
-    return check_estimator(TemplateEstimator)
-
-
-def test_classifier():
-    return check_estimator(TemplateClassifier)
-
-
-def test_transformer():
-    return check_estimator(TemplateTransformer)
+    return check_estimator(GLVQ)
