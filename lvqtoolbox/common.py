f4aa10174612169fb4fe24d76132a0e49e2911fc
Rick van Veen <r.van.veen133@gmail.com>
2018-08-13 17:26:07 +0200

    refactored the LVQClass to a common module

---
+++ lvqtoolbox/common.py
@@ -0,0 +1,22 @@
+# TODO: LVQClassifier - base class for LVQClassifier
+# Abstract classes are not Pythonian apparently. However, common functionality which we always need can still be put in
+# a common base class.
+
+import numpy as np
+
+
+def _conditional_mean(p_labels, data, d_labels):
+    return np.array([np.mean(data[p_label == d_labels, :], axis=0)
+                     for p_label in p_labels])
+
+
+def init_prototypes(p_labels, data, d_labels):
+    return _conditional_mean(p_labels, data, d_labels)
+
+
+def reshape_prototypes(prototypes, num_prototypes, num_features):
+    return prototypes.reshape([1, num_prototypes * num_features])
+
+
+def restore_prototypes(prototypes, num_prototypes, num_features):
+    return prototypes.reshape([num_prototypes, num_features])
