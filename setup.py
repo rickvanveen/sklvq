25086a0f1a4abae2147adf5c91429aa09992b179
Rick van Veen <r.van.veen133@gmail.com>
2020-01-22 10:08:18 +0100

    Updated setup.py and addition of CI for documentation

--- setup.py
+++ setup.py
@@ -1,27 +1,63 @@
-from __future__ import print_function
-import sys
-from setuptools import setup, find_packages
+#! /usr/bin/env python
 
-with open('requirements.txt') as f:
-    INSTALL_REQUIRES = [l.strip() for l in f.readlines() if l]
+import codecs
+import os
 
-try:
-    import numpy
-except ImportError:
-    print('numpy is required during installation')
-    sys.exit(1)
+from setuptools import find_packages, setup
 
-try:
-    import scipy
-except ImportError:
-    print('scipy is required during installation')
-    sys.exit(1)
+# get __version__ from _version.py
+ver_file = os.path.join('sklvq', '_version.py')
+with open(ver_file) as f:
+    exec(f.read())
 
-setup(name='sklvq',
-      version='0.0.1',
-      description='Sklearn compatible LVQ algorithms',
-      author='',
+DISTNAME = 'sklvq'
+DESCRIPTION = 'A collection of Learning Vector Quantization algorithms compatible with scikit-learn'
+with codecs.open('README.rst', encoding='utf-8-sig') as f:
+    LONG_DESCRIPTION = f.read()
+MAINTAINER = 'R. van Veen'
+MAINTAINER_EMAIL = 'r.van.veen133@gmail.com'
+URL = 'https://github.com/rickvanveen/sklvq'
+LICENSE = 'GNU Affero General Public License v3.0'
+DOWNLOAD_URL = 'https://github.com/rickvanveen/sklvq'
+VERSION = __version__
+INSTALL_REQUIRES = ['numpy', 'scipy', 'scikit-learn']
+CLASSIFIERS = ['Intended Audience :: Science/Research',
+               'Intended Audience :: Developers',
+               'License :: OSI Approved',
+               'Programming Language :: Python',
+               'Topic :: Scientific/Engineering',
+               'Topic :: Software Development',
+               'Operating System :: Microsoft :: Windows',
+               'Operating System :: POSIX',
+               'Operating System :: Unix',
+               'Operating System :: MacOS',
+               'Programming Language :: Python :: 3.5',
+               'Programming Language :: Python :: 3.6',
+               'Programming Language :: Python :: 3.7']
+EXTRAS_REQUIRE = {
+    'tests': [
+        'pytest',
+        'pytest-cov'],
+    'docs': [
+        'sphinx',
+        'sphinx-gallery',
+        'sphinx_rtd_theme',
+        'numpydoc',
+        'matplotlib'
+    ]
+}
+
+setup(name=DISTNAME,
+      maintainer=MAINTAINER,
+      maintainer_email=MAINTAINER_EMAIL,
+      description=DESCRIPTION,
+      license=LICENSE,
+      url=URL,
+      version=VERSION,
+      download_url=DOWNLOAD_URL,
+      long_description=LONG_DESCRIPTION,
+      zip_safe=False,  # the package can run out of an .egg file
+      classifiers=CLASSIFIERS,
       packages=find_packages(),
       install_requires=INSTALL_REQUIRES,
-      author_email='r.van.veen133@gmail.com',
-      )
+      extras_require=EXTRAS_REQUIRE)
