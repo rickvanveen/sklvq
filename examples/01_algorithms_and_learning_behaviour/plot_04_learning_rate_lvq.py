Removed gradient from solver state dict. Fixed waypoint averaging.

examples/01_algorithms_and_learning_behaviour/plot_04_learning_rate_lvq.py
@@ -54,7 +54,8 @@ model = GMLVQ(
     solver_params={
         "max_runs": 10,
         "k": 3,
-        "step_size": np.array([0.1, 0.05]),
+        "step_size": np.array([0.75, 0.85]), # Note we chose very large step_sizes here to show
+        # the usefulness of waypoint averaging.
         "callback": logger,
     },
     random_state=1428,
@@ -70,7 +71,9 @@ predicted_labels = pipeline.predict(data)
 print(classification_report(labels, predicted_labels))
 
 ###############################################################################
-# Additionally we can study the cost at each iteration of the solvers progress.
+# Additionally we can study the cost at each iteration of the solvers progress. Which doesn't
+# look very smooth and even gets worse. This is because of the chosen step_size, which is too
+# large.
 
 iteration, fun = zip(
     *[(state["nit"], state["fun"]) for state in logger.states]

sklvq/solvers/waypoint_gradient_descent.py
@@ -8,7 +8,7 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from sklvq.models import LVQBaseClass
 
-STATE_KEYS = ["variables", "nit", "fun", "nfun", "tfun", "njac", "tjac", "step_size"]
+STATE_KEYS = ["variables", "nit", "fun", "nfun", "tfun", "step_size"]
 
 
 # TODO: its wrong should store average of past points not gradients....
@@ -49,10 +49,6 @@ class WaypointGradientDescent(SolverBaseClass):
             The cost of the regular update step.
         - "tfun"
             The cost of the "tentative" update, i.e., the average of the past k updates.
-        - "njac"
-            The gradient update of the regular update.
-        - "tjac",
-            The average gradient update.
         - "step_size"
             The current step_size(s)
 
@@ -100,7 +96,8 @@ class WaypointGradientDescent(SolverBaseClass):
             The initial model that will be changed and holds the results at the end
 
         """
-        previous_objective_gradients = np.zeros(
+        # previous_waypoints
+        previous_waypoints = np.zeros(
             (self.k, model.to_variables(model.get_model_params()).size)
         )
 
@@ -140,13 +137,13 @@ class WaypointGradientDescent(SolverBaseClass):
             objective_gradient = model.to_variables(
                 self.multiply_model_params(step_size, objective_gradient)
             )
-
-            # Store the previous objective_gradients in variables shape
-            previous_objective_gradients[np.mod(i_run, self.k), :] = objective_gradient
-
             # Subtract objective gradient of model params in variables form
             # and
             new_model_variables = model_variables - objective_gradient
+
+            # Store the previous objective_gradients in variables shape
+            # previous_objective_gradients[np.mod(i_run, self.k), :] = objective_gradient
+            previous_waypoints[np.mod(i_run, self.k), :] = new_model_variables
 
             # Transform back to parameters form and update the model
             model.set_model_params(model.to_params(new_model_variables))
@@ -159,7 +156,6 @@ class WaypointGradientDescent(SolverBaseClass):
                     nit=i_run + 1,
                     nfun=cost,
                     fun=cost,
-                    njac=objective_gradient,  # scaled with the step_size
                     step_size=step_size,
                 )
                 if self.callback(model, state):
@@ -191,10 +187,10 @@ class WaypointGradientDescent(SolverBaseClass):
             )
 
             # Tentative update step cost
-            tentative_objective_gradient = np.mean(previous_objective_gradients, axis=0)
+            # tentative_objective_gradient = np.mean(previous_objective_gradients, axis=0)
+            tentative_model_variables = np.mean(previous_waypoints, axis=0)
 
-            tentative_model_variables = model_variables - tentative_objective_gradient
-            # tentative_model_variables = mean_previous_objective_gradients
+            # tentative_model_variables = model_variables - tentative_objective_gradient
 
             # Update the model using normalized update step
             new_model_variables = model_variables - objective_gradient
@@ -216,18 +212,16 @@ class WaypointGradientDescent(SolverBaseClass):
                 model.set_model_params(model.to_params(tentative_model_variables))
                 step_size = self.loss * step_size
                 accepted_cost = tentative_cost
-                accepted_gradient = tentative_objective_gradient
                 accepted_variables = tentative_model_variables
             else:
                 step_size = self.gain * step_size
                 accepted_cost = new_cost
-                accepted_gradient = objective_gradient
                 accepted_variables = new_model_variables
                 # We keep the model currently containing the new update -> because objective
                 # updates the model and was called last...
 
             # Administration. Store the models parameters.
-            previous_objective_gradients[np.mod(i_run, self.k), :] = accepted_gradient
+            previous_waypoints[np.mod(i_run, self.k), :] = accepted_variables
 
             if self.callback is not None:
                 state = self.create_state(
@@ -237,8 +231,6 @@ class WaypointGradientDescent(SolverBaseClass):
                     tfun=tentative_cost,
                     nfun=new_cost,
                     fun=accepted_cost,
-                    # tjac=mean_previous_objective_gradients,
-                    njac=objective_gradient,  # scaled with the step_size
                     step_size=step_size,
                 )
                 if self.callback(model, state):

sklvq/tests/test_glvq.py
@@ -16,7 +16,7 @@ class ProgressLogger:
     def __init__(self):
         self.states = np.array([])
 
-    def __call__(self, state) -> bool:
+    def __call__(self, model, state) -> bool:
         self.states = np.append(self.states, state)
         return False
 
@@ -31,7 +31,7 @@ def test_glvq_iris():
     progress_logger = ProgressLogger()
 
     classifier = GLVQ(
-        solver_type="bfgs",
+        solver_type="waypoint-gradient-descent",
         solver_params={"callback": progress_logger},
         distance_type="squared-euclidean",
         activation_type="swish",

sklvq/tests/test_gmlvq.py
@@ -30,12 +30,11 @@ def test_gmlvq_iris():
     init_omega = init_omega[0:2, :]
 
     classifier = GMLVQ(
-        solver_type="steepest-gradient-descent",
+        solver_type="waypoint-gradient-descent",
         solver_params={
             "callback": progress_logger,
             "max_runs": 20,
-            "batch_size": 1,
-            "step_size": 0.01,
+            "step_size": np.array([0.001, 0.1]),
         },
         activation_type="swish",
         distance_type="adaptive-squared-euclidean",