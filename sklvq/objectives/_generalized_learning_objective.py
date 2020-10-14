bd5f990a36c82590ec2525d2d8fa03a6b3926a21
Rick van Veen <r.van.veen133@gmail.com>
2020-10-14 14:28:51 +0200

    Update documentation and __init__ parameter checks

--- sklvq/objectives/_generalized_learning_objective.py
+++ sklvq/objectives/_generalized_learning_objective.py
@@ -6,6 +6,8 @@ from .. import activations, discriminants
 from typing import Union
 from typing import TYPE_CHECKING
 
+from .._utils import init_class
+
 if TYPE_CHECKING:
     from ..models import LVQBaseClass
 
@@ -23,6 +25,38 @@ DISCRIMINANT_FUNCTIONS = [
 
 
 class GeneralizedLearningObjective(ObjectiveBaseClass):
+    """ The generalized learning objective
+
+    Class that holds the generalized learning objective function and its gradient as described
+    in [1]_.
+
+    Parameters
+    ----------
+    activation_type : {"identity", "sigmoid", "soft-plus", "swish"} or type
+        If string needs to be one of the indicated options. If not a string needs to be a custom
+        activation class. See :class:`sklvq.activations.ActivationBaseClass`.
+    activation_params : dict or None
+        The dictionary with the parameters for the activation function or None if it doesn't
+        require any parameters.
+    discriminant_type: {"relative-distance"} or type
+        Can only be the relative distance. If not a string it can be a custom class.
+        See :class:`sklvq.discriminants.DiscriminantBaseClass`.
+    discriminant_params : dict or None
+        The dictionary with the parameters for the discriminant function or None if it doesn't
+        require any parameters.
+
+    Notes
+    -----
+    Compatible and used within the following models: :class:`.GLVQ`, :class:`.GMLVQ`,
+    and :class:`.LGMLVQ`.
+
+    References
+    ----------
+    .. [1] Sato, A., and Yamada, K. (1996) "Generalized Learning Vector Quantization."
+        Advances in Neural Network Information Processing Systems, 423–429, 1996.
+
+    """
+
     def __init__(
         self,
         activation_type: Union[str, type],
@@ -30,106 +64,99 @@ class GeneralizedLearningObjective(ObjectiveBaseClass):
         discriminant_type: Union[str, type],
         discriminant_params: dict,
     ):
-
-        if isinstance(activation_type, str):
-            activation_class = activations.import_from_string(activation_type)
-
         if activation_params is None:
             activation_params = {}
 
+        activation_class = init_class(
+            activations, activation_type, ACTIVATION_FUNCTIONS
+        )
         self.activation = activation_class(**activation_params)
-
-        if isinstance(discriminant_type, str):
-            discriminant_class = discriminants.import_from_string(discriminant_type)
 
         if discriminant_params is None:
             discriminant_params = {}
 
+        discriminant_class = init_class(
+            discriminants, discriminant_type, DISCRIMINANT_FUNCTIONS
+        )
         self.discriminant = discriminant_class(**discriminant_params)
 
     def __call__(
         self, model: "LVQBaseClass", data: np.ndarray, labels: np.ndarray,
     ) -> np.ndarray:
-        """ Computes the Generalized Learning Objective function
+        """ Computes the generalized learning objective:
+
             .. math::
 
-                E_{GLVQ} = \\Sigma_{i=1}^{N} f(\\mu(x_i))
+                E = \\sum_{i=1}^{N} f(\\mu(d_0(\\vec{x}_i), d_1(\\vec{x}_i))
 
-        with :math:`\\mu(\\cdot)` the discriminative function and :math:`f(\\cdot)` the activation
-        function.
+        with :math:`\\mu(\\cdot)` the discriminative function, :math:`f(\\cdot)` the activation
+        function, and :math:`d_0(\\vec{x}_i)` and :math:`d_1(\\vec{x}_i)` the shortest
+        distance to a prototype  with  a different and the same label respectively.
 
         Parameters
         ----------
-        variables: ndarray with shape depending on model parameters
-            Flattened 1D array of the variables that are changed, i.e., the model parameters
-
         model : LVQBaseClass
             The model which can be any LVQBaseClass compatible with this objective function.
 
         data: ndarray with shape (n_samples, n_features)
-            The X
+            The data.
 
         labels: ndarray with shape (n_samples)
+            The labels of the samples in the data.
 
         Returns
         -------
         float:
             The cost
         """
-        # model.set_model_params(model.to_params(variables))
-
         dist_same, dist_diff, _, _ = _compute_distance(data, labels, model)
-
-        # return np.sum(self.activation(self.discriminant(data, labels)))
 
         return np.sum(self.activation(self.discriminant(dist_same, dist_diff)))
 
     def gradient(
         self, model: "LVQBaseClass", data: np.ndarray, labels: np.ndarray,
     ) -> np.ndarray:
-        """ Computes the Generalized Learning Objective function's gradient
-            .. math::
-                \\frac{\\partial E_{GLVQ}}{\\partial w_0} = \\frac{\\partial f}{\\partial \\mu}
-                \\frac{\\partial \\mu}{\\partial d_0} \\frac{\\partial d_0}{\\partial w_0}
+        """ Computes the generalized learning objective's gradient with respect to the
+        prototype with a different label:
 
-        with :math:`w_0` the prototype with a different label than the X and :math:`d_0`
+            .. math::
+                \\frac{\\partial E}{\\partial \\vec{w}_0} = \\frac{\\partial f}{\\partial \\mu}
+                \\frac{\\partial \\mu}{\\partial d_0} \\frac{\\partial d_0}{\\partial \\vec{w}_0}
+
+        with :math:`\\vec{w}_0` the prototype with a different label than the data and :math:`d_0`
         the distance to that prototype.
 
             .. math::
-                 \\frac{\\partial E_{GLVQ}}{\\partial w_1} = \\frac{\\partial f}{\\partial \\mu}
-                 \\frac{\\partial \\mu}{\\partial d_1} \\frac{\\partial d_1}{\\partial w_1}
+                 \\frac{\\partial E}{\\partial \\vec{w}_1} = \\frac{\\partial f}{\\partial \\mu}
+                 \\frac{\\partial \\mu}{\\partial d_1} \\frac{\\partial d_1}{\\partial \\vec{w}_1}
 
-        with :math:`w_1` the prototype with the same label as the X and :math:`d_1`
+        with :math:`\\vec{w}_1` the prototype with the same label as the data and :math:`d_1`
         the distance to that prototype.
 
         Parameters
         ----------
-        variables: ndarray with shape depending on model parameters
-            Flattened 1D array of the variables that are changed, i.e., the model parameters
-
         model : LVQBaseClass
             The model which can be any LVQBaseClass compatible with this objective function.
 
         data: ndarray with shape (n_samples, n_features)
-            The X
+            The data.
 
         labels: ndarray with shape (n_samples)
+            The labels of the samples in the data.
 
         Returns
         -------
-            ndarray with shape of input variables
-                The generalized learning objective function's gradient
+        ndarray with the same shape as the model variables array (depending on the model)
+            The generalized learning objective function's gradient
 
         """
+
         dist_same, dist_diff, i_dist_same, i_dist_diff = _compute_distance(
             data, labels, model
         )
-
-        # Distances plus dist_same / dist_diff indices...
         discriminant_score = self.discriminant(dist_same, dist_diff)
 
-        # Gradient is basically model._variables... way to view params but also single
-        # prototype/omega?
+        # Pre-allocation, needs to be zero.
         gradient_buffer = np.zeros(model.get_variables().size)
 
         # For each prototype
@@ -138,7 +165,7 @@ class GeneralizedLearningObjective(ObjectiveBaseClass):
             # ii_winner_same = i_prototype == i_dist_same
             if i_prototype in i_dist_same:
                 ii_winner_same = i_prototype == i_dist_same
-                # Only if these cases exist we can compute an update
+                # Only if these cases exist we can/should compute an update
                 self._partial_gradient(
                     gradient_buffer,
                     discriminant_score[ii_winner_same],
@@ -186,7 +213,7 @@ class GeneralizedLearningObjective(ObjectiveBaseClass):
         )
 
         # Computes the following partial derivatives: ddi/dwi, with i = 2
-        distance_gradient = model._distance.gradient(data, model, i_prototype)
+        distance_gradient = model.distance.gradient(data, model, i_prototype)
 
         # The distance vectors weighted by the activation and discriminant partial
         # derivatives.
@@ -199,9 +226,11 @@ class GeneralizedLearningObjective(ObjectiveBaseClass):
 
 def _find_min(indices: np.ndarray, distances: np.ndarray) -> (np.ndarray, np.ndarray):
     """ Helper function to find the minimum distance and the index of this distance. """
+    # Set the irrelevant distances to infinity.
     dist_temp = np.where(indices, distances, np.inf)
-
+    # Find the indices of the closest prototype (column)
     i_dist_min = dist_temp.argmin(axis=1)
+    # Return the shortest distances and the indices of the prototypes.
     return dist_temp[np.arange(i_dist_min.size), i_dist_min], i_dist_min
 
 
@@ -212,7 +241,7 @@ def _compute_distance(data: np.ndarray, labels: np.ndarray, model: "LVQBaseClass
 
     # Step 1: Compute distances between X and the model (how is depending on model and coupled
     # distance function)
-    distances = model._distance(data, model)
+    distances = model.distance(data, model)
 
     # Step 2: Find for all samples the distance between closest prototype with same label (d1)
     # and different label (d2). ii_same marks for all samples the prototype with the same label.
@@ -220,13 +249,17 @@ def _compute_distance(data: np.ndarray, labels: np.ndarray, model: "LVQBaseClass
     num_samples = labels.size
     num_prototypes = model.prototypes_labels_.size
     if num_samples == 1:
+        # Faster if num_samples == 1
         ii_same = np.atleast_2d(labels == prototypes_labels)
     elif num_samples < num_prototypes:
+        # Faster to go over the labels if there are less then the prototypes.
         ii_same = np.array([label == prototypes_labels for label in labels])
     else:
+        # List comprehension of the prototypes. This are all slight improvements to computation
+        # time, as list comprehension takes quite some time.
         ii_same = np.transpose(
             [labels == prototype_label for prototype_label in prototypes_labels]
-        )  # List comprehension takes 13% of total computation time in compute distance...
+        )
 
     # For each prototype mark the samples that have a different label
     ii_diff = ~ii_same
