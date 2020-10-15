4f068b6947a17b2fbf57aef2dec3f09e12b8775b
Rick van Veen <r.van.veen133@gmail.com>
2020-10-15 14:06:24 +0200

    Documentation update

--- sklvq/solvers/_steepest_gradient_descent.py
+++ sklvq/solvers/_steepest_gradient_descent.py
@@ -28,13 +28,13 @@ class SteepestGradientDescent(SolverBaseClass):
 
     max_runs: int
         Maximum number of runs/epochs that will be computed. Should be >= 1. Early stopping can
-        be implemented by providing a callback function that returns True when the solver should
-        stop. See callback documentation below.
+        be implemented by providing a ``callback`` function that returns True when the solver should
+        stop.
 
     batch_size: int
         Controls the batch size and accepts a value >= 0. The value indicates the number of
         samples considered to be in the batch. A stochastic gradient descent corresponds with a
-        batch_size of 1. For Batch gradient descent 0 can be used to indicate to use all the
+        ``batch_size`` of 1. For Batch gradient descent 0 can be used to indicate to use all the
         samples. Any value > 1 < n_samples can be considered as a mini-batch gradient descent.
 
         If batches can not properly  be divided in batches with the specified size the last
@@ -43,16 +43,14 @@ class SteepestGradientDescent(SolverBaseClass):
         The data is always shuffled before it is split into batches.
 
     step_size: float or ndarray
-        The step size to control the learning rate of the model parameters. If the same step_size
+        The step size to control the learning rate of the model parameters. If the same ``step_size``
         should be used for all parameters (e.g., prototypes and omega) then a single float is
-        sufficient. If separate initial step_sizes should be used per model parameter then this
-        should be specified by using a ndarray.
-
-        The step size(s) are reduced over time:  step_size / (t  / max_runs)
+        sufficient. If separate initial step sizes should be used per model parameter then this
+        should be specified by using a numpy array.
 
     callback: callable
         Callable with signature callable(state). If the callable returns True the solver
-        will stop even if max_runs is not reached yet. The state object contains the following:
+        will stop even if ``max_runs`` is not reached yet. The state object contains the following:
 
         - "variables"
             Concatenated 1D ndarray of the model's parameters
@@ -70,7 +68,7 @@ class SteepestGradientDescent(SolverBaseClass):
         objective: ObjectiveBaseClass,
         max_runs: int = 10,
         batch_size: int = 1,
-        step_size: float = 0.2,
+        step_size: float = 0.1,
         callback: callable = None,
     ):
         super().__init__(objective)
@@ -107,9 +105,24 @@ class SteepestGradientDescent(SolverBaseClass):
     def solve(
         self, data: np.ndarray, labels: np.ndarray, model: "LVQBaseClass",
     ):
-        """ Solve
+        """ Solve function that gets called by the fit method of the models.
 
-        Performs the actual steps of the gradient descent solvers.
+        Performs the following update of the model parameters (:math:`\\theta`) per batch. This
+        process is repeated multiple times (per step) when the ``batch_size`` (:math:`M`) is smaller
+        than the total number of samples in the data.
+
+        .. math::
+            \\theta = \\theta  - \\eta(t) \\cdot \\sum_i^M \\nabla  e_i(\\theta),
+
+        with :math:`\\nabla e_i(\\theta)` the gradient of the objective function with respect to
+        a sample given the current model parameters :math:`\\theta`, and :math:`\\eta(t)` the step
+        size at step :math:`t`, which used a simple annealing function:
+
+        .. math::
+            \\eta(t) = \\frac{\\eta_{init}} {(1 + \\frac{t}{t_{max}})},
+
+        with :math:`t_{max}` given by the ``max_runs`` parameter and :math:`\\eta_{init}` by the
+        ``step_size`` parameter.
 
         Parameters
         ----------
