96c6c4ca47b5feefc3318ad1d3470a91bfe985fa
Rick van Veen <r.van.veen133@gmail.com>
2024-10-27 12:36:48 +0100

    move code to src/sklvq and tests outside the package

--- sklvq/__init__.py
+++
@@ -1,4 +0,0 @@
-from .models import GLVQ, GMLVQ, LGMLVQ
-from ._version import __version__
-
-__all__ = ["GLVQ", "GMLVQ", "LGMLVQ", "__version__"]
--- sklvq/objectives/__init__.py
+++
@@ -1,4 +0,0 @@
-from ._base import ObjectiveBaseClass
-from ._generalized_learning_objective import GeneralizedLearningObjective
-
-__all__ = ["ObjectiveBaseClass", "GeneralizedLearningObjective"]
